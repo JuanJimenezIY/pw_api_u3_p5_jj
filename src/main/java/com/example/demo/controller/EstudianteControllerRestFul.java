eb97c18b700de2d2f440c1736f2f39d47d2d9fcd
Juan Jimenez <sr.j_599@hotmail.com>
2024-01-24 20:44:42 -0500

    taller26

---
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+//API: por el proyecto java
+
+//Servicio-> Controller: Clase Controller
+
+@RestController  //Servicio
+
+public class EstudianteControllerRestFul {
+	
+	//Metodos: capacidades
+	public void guardar(Estudiante estudiante) {
+		
+		
+	}
+
+}
