413595c68eb3d8e19f12b0ee7a151079d3bfef28
Juan Jimenez <sr.j_599@hotmail.com>
2024-02-07 20:34:46 -0500

    taller31

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,14 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.repository.modelo.Estudiante;
-import com.example.demo.service.IEstudianteService;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,7 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
 
 //API: por el proyecto java
 
@@ -36,7 +39,7 @@ public class EstudianteControllerRestFul {
 	// Metodos: capacidades
 	
 	
-	@PostMapping
+	@PostMapping(consumes = MediaType.APPLICATION_XML_VALUE )
 	public void guardar(@RequestBody Estudiante estudiante) {
 		this.iEstudianteService.guardar(estudiante);
 
@@ -44,22 +47,34 @@ public class EstudianteControllerRestFul {
 	// http://localhost:8080/API/v1.0/Matricula/estudiantes
 
 	// para decirle que va a manejar el verbo GET con la anotacion
-	@GetMapping(path = "/{id}")
-	public Estudiante buscar(@PathVariable Integer id) {
-		return this.iEstudianteService.buscar(id);
+	@GetMapping(path = "/{id}", produces = "application/xml")
+	public  ResponseEntity<Estudiante>  buscar(@PathVariable Integer id) {
+		
+		//240: grupo satisfactorio 
+		//240: Recurso Estudiante encontrado satisfactoriamente
+		Estudiante estu = this.iEstudianteService.buscar(id);
+		//200 OK
+		//contrato de la API (documento,pdf, Swagger.io)
+		
+		
+		return ResponseEntity.status(HttpStatus.OK).body(estu);
 		// http://localhost:8080/API/v1.0/Matricula/estudiantes/{id} GET
 	}
 
 	//para consultar todo no necesito 
 	//@GetMapping(path = "/consultarTodos")
-	@GetMapping
-	public List<Estudiante> consultarTodos(@RequestParam(required = false,defaultValue = "masculino") String genero) {
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public  ResponseEntity<List<Estudiante>>  consultarTodos(@RequestParam(required = false,defaultValue = "masculino") String genero) {
 		
-		return this.iEstudianteService.consultarTodos(genero);
+		List<Estudiante> lista = this.iEstudianteService.consultarTodos(genero);
+		HttpHeaders cabeceras = new HttpHeaders();
+		cabeceras.add("mensaje_242", "Lista consultada de manera satisfactoria");
+		cabeceras.add("mensaje_info", "El sistema va a estar en mantenimiento el fin de semana");
+		return new ResponseEntity<>(lista, cabeceras,242) ;
 		// http://localhost:8080/API/v1.0/Matricula/estudiantes/consultarTodos?genero=masculino&edad=100
 	}
 
-	@PutMapping(path = "/{id}")
+	@PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void actualizar(@RequestBody Estudiante estudiante,@PathVariable Integer id) {
 		estudiante.setId(id);
 		this.iEstudianteService.actualizar(estudiante);
@@ -67,7 +82,7 @@ public class EstudianteControllerRestFul {
 		// http://localhost:8080/API/v1.0/Matricula/estudiantes/{id} PUT parametro para saber que estoy actualizando ese estudiante
 	}
 
-	@PatchMapping(path = "/{id}")
+	@PatchMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void actualizarParcial(@RequestBody Estudiante estudiante,@PathVariable Integer id) {
 		estudiante.setId(id);
 		this.iEstudianteService.actualizarParcial(estudiante.getApellido(), estudiante.getNombre(), id);
